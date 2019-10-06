240e69b0125f3c7461e6e96a26e64bbe7b3b9a1b
Jeremy Gill <jeremygill87@gmail.com>
2019-10-05 21:06:36 -0400

    Added music and some win/loss logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,50 @@
-        var words = ["pizza", "leonardo", "donatello", "michelangelo", "raphael", "shredder", "krang"];
-        var target = (Math.floor(Math.random() * words.length));
-        var targetWord = words[target];
-        var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-        var answers = [ ];
+$(document).ready(function hangmanGame() {
+    var words = ["pizza", "leonardo", "donatello", "michelangelo", "raphael", "shredder", "krang"];
+    var target = (Math.floor(Math.random() * words.length));
+    var targetWord = words[target];
+    var spaces = "_ ";
+    var blankWord = [];
+    var letters = [];
+    var guess = "";
+    var win = 0;
+    var loss = 0;
 
-        document.onkeyup = function(event) {
-            var spaces = "_ ";
-            for (var i = 0; i < targetWord.length; i++)
-        }
-            var guess = event.key;
+    //guess array that holds letters that users have already guessed
+    $("#blanks").append(spaces.repeat(targetWord.length)) ;
 
-        
+    for (var i = 0; i < targetWord.length; i++ ){
+        blankWord.push("_ ");
+    }
+    
+
+    document.onkeyup = function(event) {
+        guess = event.key;
+        (letters).push(guess);
+        $("#letters-guessed").text(letters);
+
+    if (targetWord.indexOf(guess) !== -1){
         for (var i = 0; i < targetWord.length; i++) {
-            answers[i] = "_ ";
-        }
-
-        var remainingLetters = targetWord.length;
-        
-        while (remainingLetters > 0) {
-            var guess = prompt("Guess a letter, or type 'exit' to stop playing");
-            if (guess === "exit") {
-                break;
-            } else {
-                for (var j = 0; j < words.length; j++) {
-                    answers[j] = guess;
-                    remainingLetters--;
-                }
+            if (targetWord [i] === guess){
+                blankWord[i] = guess;
             }
+            $("#blanks").text(blankWord.join(""));
         }
     }
+   var blankWordString = blankWord.join("");
+   for (j = 0; j < 10; j++){
+   if (blankWordString === targetWord && j <= 10) {
+       win++;
+       console.log("Wins: " + win)
+       $("#win-loss-record").text("Wins: " + win + " Losses: " + loss);
+   }
+   else if (blankWordString !== targetWord && j == 10) {
+       loss++;
+       console.log("Losses: " + loss);
+       $("#win-loss-record").text("Wins: " + win + " Losses: " + loss);
+   }
+    console.log("Wins: " + win);
+    console.log("Losses: " + loss);
+    }
+    }
+});
    
